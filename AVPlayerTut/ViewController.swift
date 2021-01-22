290ef07d1af6178969636e5d9674ef5ee7fdbc1d
Henry Chukwu <henrychukwu13@gmail.com>
2021-01-22 08:06:16 +0100

    Initial Commit

---
+++ AVPlayerTut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVPlayerTut
+//
+//  Created by Henry Chukwu on 22/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
